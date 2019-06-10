8500d33ac559f73952e2289ae5f7fe392fc8d086
Blake Bacon <blakephudson@gmail.com>
2019-06-10 08:56:28 -0400

    got apis running

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get("/", (req, res) =>
   res.send("api running! & i made change, then i made another")
 );
 
+// Define Routes
+
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
