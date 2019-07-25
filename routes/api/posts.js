3b12f5f2a7893555647ff4e5c96240245391e62b
Blake Hudson <bhuzschs@gmail.com>
2019-07-24 22:03:47 -0400

    added all of profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -47,4 +47,81 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all post
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get single post by ID
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    DELETE post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+
+/*
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has already been liked
+    if(post.likes.filter(like => ))
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+    
+  }
+}); */
+
 module.exports = router;
--- server.js
+++ server.js
@@ -23,12 +23,3 @@ app.use("/api/users", require("./routes/api/users"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-/*
-
-const connectDB = require("./config/db");
-
-// Connect Database
-connectDB();
-
-*/
